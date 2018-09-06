be66eb77946821bfbb1d786041f9598926f17b23
cuiiug <myjava_cn@126.com>
2018-09-06 22:03:21 +0800

    添加注释

--- src/main/java/cn/sample/server/RpcBootstrap.java
+++ src/main/java/cn/sample/server/RpcBootstrap.java
@@ -9,6 +9,11 @@ public class RpcBootstrap {
 
 	public static void main(String[] args) {
 		LOGGER.debug("start server");
+		// 启动spring容器
+		/**
+		 * Spring容器启动过程，会加载相应的bean; RpcServer实现了InitializingBean接口，初始化bean时
+		 * 执行afterPropertiesSet方法
+		 */
 		new ClassPathXmlApplicationContext("spring.xml");
 	}
 }
